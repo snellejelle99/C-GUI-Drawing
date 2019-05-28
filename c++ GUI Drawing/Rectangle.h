4e4ab0cc693eec76bb241d05cb896f27ab150c83
snellejelle99 <snellejelle99jb@gmail.com>
2019-05-28 10:06:14 +0200

    correcte indents voor saven

--- c++ GUI Drawing/Rectangle.h
+++ c++ GUI Drawing/Rectangle.h
@@ -15,7 +15,7 @@ public:
 	virtual Windows::UI::Xaml::Shapes::Shape^ GetShape();
 	virtual void Accept(ShapeDeleteVisitor shapeDeleteVisitor);
 	virtual void Accept(ShapeAddVisitor shapeAddVisitor);
-	virtual Platform::String^ ToString();
+	virtual Platform::String^ ToString(int counter);
 
 private:
 	Windows::UI::Xaml::Shapes::Rectangle ^rect;
--- c++ GUI Drawing/Shape.h
+++ c++ GUI Drawing/Shape.h
@@ -23,7 +23,7 @@ public:
 	virtual void ChangeColor(Windows::UI::Color newColor) = 0; //changes the color of the shape
 	virtual bool CheckShape(Windows::UI::Xaml::Shapes::Shape^ shape) = 0;
 	virtual Windows::UI::Xaml::Shapes::Shape^ GetShape() = 0;
-	virtual Platform::String^ ToString() = 0; //returns a string with parameters of the object
+	virtual Platform::String^ ToString(int counter) = 0; //returns a string with parameters of the object
 
 protected:
 	double left;
